1b16a3fdefd01b67823afced093a7eeb3fd3edaa
Anil2406 <Anil.Shirole.jy@gmail.com>
2018-03-18 17:26:54 +0530

    changed swagger docs

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var swaggerUi = require('swagger-ui-express');
 
 var db = mongoose.connect('mongodb://127.0.0.1:27017/movieAPI');
 var Movie = require('./server/models/movieModel');
 var logger = require('./server/common/log.conf');
-var swagger = require('./server/common/swagger');
+var swaggerDocument = require('./server/common/swagger/swagger.json');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(logger);
-app.use(express.static('./server/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var movieRouter = require('./server/Routes/movieRoutes')(Movie);
 
-app.use('/api/movies', movieRouter);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-swagger.configureSwagger(app, function(swaggerApp) {
-    app = swaggerApp;
-});
+app.use('/api/movies', movieRouter);
 
 
 app.listen(port, function() {
